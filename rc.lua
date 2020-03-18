b634d2c54549b6a61531073d30c1f56c0fe029b2
Jesus Fernandez <jsfdez@gmail.com>
2020-03-18 16:07:20 +0100

    Change help hotkey

--- rc.lua
+++ rc.lua
@@ -372,7 +372,7 @@ end
 -- {{{ Key bindings
 globalkeys =
     awful.util.table.join(
-    awful.key({modkey}, 's', hotkeys_popup.show_help, {description = 'show help', group = 'awesome'}),
+    awful.key({modkey}, 'F1', hotkeys_popup.show_help, {description = 'show help', group = 'awesome'}),
     awful.key({modkey}, 'Left', awful.tag.viewprev, {description = 'view previous', group = 'tag'}),
     awful.key({modkey}, 'Right', awful.tag.viewnext, {description = 'view next', group = 'tag'}),
     awful.key({modkey}, 'Escape', awful.tag.history.restore, {description = 'go back', group = 'tag'}),
